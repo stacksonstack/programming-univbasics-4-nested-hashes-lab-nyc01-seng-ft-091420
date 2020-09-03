95bf7dfede3fbfd828243e01fe0af5d65b1255e4
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-03 01:33:44 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,6 +82,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["C", "LISP"]
+    }
+    
+    programmer_hash
 
 end
 
@@ -103,6 +111,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    programmer_hash
 
 end
 
@@ -125,5 +137,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    
+    programmer_hash
 
 end
