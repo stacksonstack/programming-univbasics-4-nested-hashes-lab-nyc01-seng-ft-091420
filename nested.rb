363496ff75105694fa45211902b7b6a21aebea70
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-03 01:26:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,10 +85,10 @@ def adding_matz
     
     programmer_hash[:yukihiro_matsumoto] = {
       :known_for => "Ruby",
-      :languages => "C"
+      :languages => ["C", "LISP"]
     }
     
-    programmer_hash[:yukihiro_matsumoto]
+    programmer_hash
 
 end
 
